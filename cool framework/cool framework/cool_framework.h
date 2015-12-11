d8456eb7538e9536618a68e17eea329352deceec
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2015-12-11 17:26:02 -0500

    add working carthage project

---
+++ cool framework/cool framework/cool_framework.h
@@ -0,0 +1,19 @@
+//
+//  cool framework.h
+//  cool framework
+//
+//  Created by Pierre-Marc Airoldi on 2015-12-11.
+//  Copyright © 2015 Pierre-Marc Airoldi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cool framework.
+FOUNDATION_EXPORT double cool_frameworkVersionNumber;
+
+//! Project version string for cool framework.
+FOUNDATION_EXPORT const unsigned char cool_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cool_framework/PublicHeader.h>
+
+
